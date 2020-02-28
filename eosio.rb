56800fc9c86fde69cda70cb18134a5f99d157558
Matthieu Vachon <matt@dfuse.io>
2020-02-28 12:16:49 -0500

    Initial commit

---
+++ eosio.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class Eosio < Formula
+  homepage 'https://github.com/eosio'
+  revision 0
+  url 'https://github.com/eosio/eos/archive/v2.0.3-dm.tar.gz'
+  version '2.0.3-dm'
+
+  option :universal
+
+  depends_on 'gmp'
+  depends_on 'gettext'
+  depends_on 'openssl@1.1'
+  depends_on 'libusb'
+  depends_on macos: :mojave
+  depends_on arch: :intel
+
+  bottle do
+    root_url 'https://github.com/eosio/eos/releases/download/v2.0.3-dm'
+    sha256 '9fe0cc75c9e9ca440b175c77008648074abed18a521e739ccffb0bf0be56cabc' => :mojave
+  end
+  def install
+    raise 'Error, only supporting binary packages at this time'
+  end
+end
+__END__
