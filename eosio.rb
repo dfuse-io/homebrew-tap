230c77fb9cd0605ad5119d78a9f2bb65e995f9bc
Matthieu Vachon <matt@dfuse.io>
2020-04-22 16:18:14 -0400

    Updated to eosio formula v2.0.5-dm

--- eosio.rb
+++ eosio.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class Eosio < Formula
-  homepage 'https://github.com/dfuse-io'
+  homepage 'https://github.com/dfuse-io/eos'
   revision 0
-  url 'https://github.com/dfuse-io/eos/archive/v2.0.3-dm.tar.gz'
-  version '2.0.3-dm'
+  url 'https://github.com/dfuse-io/eos/archive/v2.0.5-dm.tar.gz'
+  version '2.0.5-dm'
 
   option :universal
 
@@ -16,8 +16,8 @@ class Eosio < Formula
   depends_on arch: :intel
 
   bottle do
-    root_url 'https://github.com/dfuse-io/homebrew-tap/releases/download/eosio-v2.0.3-dm'
-    sha256 '9fe0cc75c9e9ca440b175c77008648074abed18a521e739ccffb0bf0be56cabc' => :mojave
+    root_url 'https://github.com/dfuse-io/eos/releases/download/v2.0.5-dm'
+    sha256 '923dad2d7f77c62db22ac6fb4b87f37500f72b0abfb8d170abe6f7f30ff9bf9c' => :mojave
   end
   def install
     raise 'Error, only supporting binary packages at this time'
