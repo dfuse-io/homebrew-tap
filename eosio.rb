f3cc1315b46ed4a23c3a70994bc7685b2fd4c9e4
Matthieu Vachon <matthieu.vachon@dfuse.io>
2021-07-20 18:42:52 -0400

    Ran brew style --fix on eosio formulae

--- eosio.rb
+++ eosio.rb
@@ -1,26 +1,27 @@
+# typed: false
 # frozen_string_literal: true
 
 class Eosio < Formula
-  homepage 'https://github.com/dfuse-io/eos'
+  homepage "https://github.com/dfuse-io/eos"
+  url "https://github.com/dfuse-io/eos/archive/v2.0.8-dm.12.0.tar.gz"
+  version "2.0.8-dm.12.0"
   revision 0
-  url 'https://github.com/dfuse-io/eos/archive/v2.0.8-dm.12.0.tar.gz'
-  version '2.0.8-dm.12.0'
+
+  bottle do
+    root_url "https://github.com/dfuse-io/eos/releases/download/v2.0.8-dm-12.0"
+    sha256 mojave: "094c4c62ed312a3c019e900652d883140f589c9cee71d70a65148630f905ebaf"
+  end
 
   option :universal
 
-  depends_on 'gmp'
-  depends_on 'gettext'
-  depends_on 'openssl@1.1'
-  depends_on 'libusb'
-  depends_on macos: :mojave
   depends_on arch: :intel
-
-  bottle do
-    root_url 'https://github.com/dfuse-io/eos/releases/download/v2.0.8-dm-12.0'
-    sha256 '094c4c62ed312a3c019e900652d883140f589c9cee71d70a65148630f905ebaf' => :mojave
-  end
+  depends_on "gettext"
+  depends_on "gmp"
+  depends_on "libusb"
+  depends_on macos: :mojave
+  depends_on "openssl@1.1"
   def install
-    raise 'Error, only supporting binary packages at this time'
+    raise "Error, only supporting binary packages at this time"
   end
 end
 __END__
